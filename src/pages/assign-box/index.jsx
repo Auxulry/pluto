ad8ea53a4fcb4b9a8ad271ee21f2176c43a66484
Mochamad Akbar <mochakbar912@gmail.com>
2024-09-17 17:56:41 +0700

    initial commit

--- src/pages/assign-box/index.jsx
+++ src/pages/assign-box/index.jsx
@@ -1,13 +1,19 @@
 import {IoIosQrScanner, IoMdArrowDropdown, IoMdNotifications} from "react-icons/io";
 import {MdOutlineArrowOutward, MdOutlineKeyboardArrowDown, MdOutlineKeyboardArrowUp} from "react-icons/md";
-import {useEffect, useRef, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {IoAddCircleOutline, IoEye, IoFilter, IoFilterOutline} from "react-icons/io5";
 import {FaFilter, FaRegEnvelope, FaSearch} from "react-icons/fa";
 import MainLayout from "@/components/layouts/MainLayout";
 import OperatorDashboard from "@/components/organisms/operator/OperatorDashboard";
 import ReviewerDashboard from "@/components/organisms/reviewer/ReviewerDashboard";
+import TextField from "@/components/atoms/text-field/TextField";
+import Modal from "@/components/atoms/modal/Modal";
+import Alert from "@/components/atoms/alert/Alert";
 
-const DataTable = () => {
+const DataTable = ({
+  setOpen,
+  setSelectedActor
+}) => {
   // State to toggle dropdown visibility
   const [dropdownOpen, setDropdownOpen] = useState(null);
 
@@ -36,6 +42,11 @@ const DataTable = () => {
     };
   }, [dropdownOpen]);
 
+  const handleModalClicked = (name) => {
+    setOpen(true);
+    setSelectedActor(name);
+  }
+
   return (
     <div className="container mx-auto">
       <div className="overflow-x-auto bg-white">
@@ -45,14 +56,21 @@ const DataTable = () => {
             <th className="px-6 py-3 text-left text-gray-600">No</th>
             <th className="px-6 py-3 text-left text-gray-600">Nama Operator</th>
             <th className="px-6 py-3 text-left text-gray-600">Jumlah Assign Box</th>
+            <th className="px-6 py-3 text-left text-gray-600"></th>
           </tr>
           </thead>
           <tbody>
           {[1, 2].map((row, index) => (
             <tr key={index} className="border-b">
-              <td className="px-6 py-4 text-gray-700">{index+1}</td>
+              <td className="px-6 py-4 text-gray-700">{index + 1}</td>
               <td className="px-6 py-4 text-gray-700">John Doe</td>
               <td className="px-6 py-4 text-gray-700">250</td>
+              <td
+                className="px-6 py-4 text-gray-700 underline cursor-pointer"
+                onClick={() => handleModalClicked("John Doe")}
+              >
+                Assign Box
+              </td>
             </tr>
           ))}
           </tbody>
@@ -74,9 +92,28 @@ const DataTable = () => {
 };
 
 export default function AssignBox() {
+  const [isOpen, setIsOpen] = useState(false)
+  const [selectedActor, setSelectedActor] = useState('')
+  const [showAlert, setShowAlert] = useState(false)
+
+
+  const handleSubmit = () => {
+    setIsOpen(false)
+    setSelectedActor('')
+    setShowAlert(true)
+  }
+
   return (
     <MainLayout>
       <div className='container mx-auto'>
+        {showAlert && (
+          <Alert
+            type="success"
+            title="Success!"
+            message="Permintaan Assign Box berhasil dikirim."
+            onClose={() => setShowAlert(false)}
+          />
+        )}
         <div className='flex flex-row items-center justify-between my-4'>
           <div>
             <h2 className="text-lg font-bold">Assign Box</h2>
@@ -111,7 +148,43 @@ export default function AssignBox() {
         </div>
 
         {/*/!* Table *!/*/}
-        <DataTable/>
+        <DataTable
+          setOpen={setIsOpen}
+          setSelectedActor={setSelectedActor}
+        />
+
+        <Modal
+          isOpen={isOpen}
+          onClose={() => setIsOpen(false)}
+          width='w-1/3'
+        >
+          <h2 className="text-2xl font-bold mb-4">Assign Box</h2>
+          <div className="mb-4">
+            <div className='flex flex-col gap-1.5 items-start my-4'>
+              <h6 className='text-sm'>Nama Operator</h6>
+              <h6 className='text-sm font-bold'>{selectedActor}</h6>
+            </div>
+            <TextField
+              type="number"
+              label='Jumlah Box'
+              placeholder='Input Jumlah Box'
+            />
+          </div>
+          <div className='flex flex-row-reverse items-center gap-4'>
+            <button
+              className='px-4 py-2 rounded focus:outline-none font-medium bg-green-500 text-white hover:bg-green-600'
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+            <button
+              className='px-4 py-2 rounded focus:outline-none font-medium text-black hover:text-gray-600'
+              onClick={() => setIsOpen(false)}
+            >
+              Cancel
+            </button>
+          </div>
+        </Modal>
       </div>
     </MainLayout>
   );
