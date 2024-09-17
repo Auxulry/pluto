c5c6ec0b1001cd7876b022958ea29366e07eae4f
sniren210 <sniren2002@gmail.com>
2024-09-17 18:07:32 +0700

    feat: make input all

---
+++ src/pages/extraction/input.js
@@ -0,0 +1,7 @@
+import InputExtractionContainer from "@/containers/extraction/InputExtractionContainer";
+
+function InputExtraction() {
+  return <InputExtractionContainer />;
+}
+
+export default InputExtraction;
---
+++ src/pages/tax-calculation/input.js
@@ -0,0 +1,7 @@
+import InputTaxCalculationContainer from "@/containers/taxCalculation/InputTaxCalculationContainer";
+
+function InputTaxCalculation() {
+  return <InputTaxCalculationContainer />;
+}
+
+export default InputTaxCalculation;
